3a891630c4b26c88240c78a2d08af7cb6043fe54
IikanBonilla <iikanyoharybonilla@gmail.com>
2026-02-17 22:55:43 -0500

    Carga inicial (Conexión DB y Dependencias)

---
+++ src/main/java/Development/PahandlerApplication.java
@@ -0,0 +1,13 @@
+package Development;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PahandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PahandlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paapp/appointmentshandlerAPP/PahandlerV0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paapp.appointmentshandlerAPP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PahandlerV0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
