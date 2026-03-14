ae7764cd1e759a2ee56a763f3cbcb3ec7ec565d6
IikanBonilla <iikanyoharybonilla@gmail.com>
2026-03-13 22:54:35 -0500

    Authentification and appointment, CSV funcition and JAVAFX is missing Also observer and some about GOF

---
+++ src/main/java/Development/Model/Appointment.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Development.Model;
+
+import Development.Model.Gender;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author iikan
+ */
+@Entity
+@Table(name = "appointment")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Appointment{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    
+    private String identificacion;
+    
+    private String fullName;
+    
+    private String professionalName;
+    
+    private String phoneNumber;
+    
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    
+    private LocalDate date;
+    
+    private LocalTime time;  
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+  
+    
+}
