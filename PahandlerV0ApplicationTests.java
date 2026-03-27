d9ecee4b7259e15ff380ca619fd464850f56d369
andreafgomezs <andreafgomez@unicauca.edu.co>
2026-03-27 11:59:28 -0500

    Add files via upload

---
+++ PahandlerV0ApplicationTests.java
@@ -0,0 +1,14 @@
+package AppointmentTests;
+
+import Development.PahandlerApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = PahandlerApplication.class) 
+class PahandlerV0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
